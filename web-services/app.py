f862888ebe0c392d5f38749f8b7b9e843710af34
kris0044 <krismistry74@gmail.com>
2025-09-12 16:24:22 +0530

    initial microservices setup

---
+++ web-services/app.py
@@ -0,0 +1,14 @@
+import requests
+import os
+from flask import Flask
+app = Flask(__name__)
+ANALYTICS_URL=os.environ.get("ANALYTICS_URL","http://analytics:5001/visit")
+@app.route('/')
+def home():
+  requests.get(ANALYTICS_URL)
+  return "hello from web services!"
+@app.route('/about')
+def about():
+  return "web services in microservices demo"
+if __name__ == "__main__":
+  app.run(host='0.0.0.0',port=5000)
