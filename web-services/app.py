eb347cd7010c38ccea67fea6d4b7d56420bf6fe8
kris0044 <krismistry74@gmail.com>
2025-09-13 16:02:33 +0530

    new root

--- web-services/app.py
+++ web-services/app.py
@@ -1,18 +1,22 @@
+from flask import Flask
 import requests
 import os
-from flask import Flask
+
 app = Flask(__name__)
-ANALYTICS_URL=os.environ.get("ANALYTICS_URL","http://analytics:5001/visit")
+ANALYTICS_URL = os.environ.get("ANALYTICS_URL", "http://analytics:5001/visit")
+
 @app.route('/')
 def home():
-  requests.get(ANALYTICS_URL)
-  return "hello from web services!"
+    requests.get(ANALYTICS_URL)
+    return "Hello from Web Service!"
+
 @app.route('/about')
 def about():
-  return "web services in microservices demo"
-@app.route('/newroot')
-def newroot():
-	return "this is new root"
+    return "Web Service in Microservices Demo"
+
+@app.route('/cicd')
+def cicd():
+    return "CI/CD using Git Actions with Docker"
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0',port=5000)
+    app.run(host='0.0.0.0', port=5000)
